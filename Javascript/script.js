c16e5c9f9efa2a8265378ea8e556fa223afc5ca3
Revenant@2 <adoyimathias942@gmail.com>
2025-05-09 01:23:22 +0100

    my Assignment

---
+++ Javascript/script.js
@@ -0,0 +1,35 @@
+//  for (let i = 3; i <= 100; i +=2){
+//     console.log(i);
+// }
+
+
+
+
+// let s = 3; 
+// while( s <= 100 ){
+//     console.log(s);
+//     s+=2
+// }
+   
+
+
+ for (let v = 1; v <= 100; v +=1){
+
+    if(v % 3 == 0){
+        console.log("fizz");
+
+    }
+    else if(v % 5 ==0){
+        console.log('buzz')
+    }
+    else if(v % 3 & 5 == 0){
+        console.log('fizzbuzz')
+    }
+    else(
+        console.log(v)
+    )
+    
+    
+
+    
+}
